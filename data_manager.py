abcf7b641dece766ad3e5dc0ba9e83fa795b1eea
kosuke miyoshi <miyoshi@narr.jp>
2018-09-20 17:22:17 +0900

    preparing data manager

---
+++ data_manager.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import pickle
+
+# データ内の1エピソードのサイズ
+EPISODE_LENGTH = 400
+
+
+class DataManager(object):
+    def __init__(self):
+        data = pickle.load(open("data/data.p", "rb"))
+        input_x = data["x"] # (49999, 7)
+        # angle(-1~1), vel_x, vel_z, angular_vel, velocity_magnigude, heading?, velocity?
+        # vel_x, vel_zからvelocity_magnitudeに等しいものが出るのは確認できている.
+        # headingはよくわからない
+        # velocityは、velocity_magnitudeの1/5に等しい
+        
+        input_y = data["y"] # (49999, 2)
+        # pos_x, pos_z
+        # posは、-4.03 ~ +4.03あたりの範囲になっている.
+
+        self.velocity_xs = input_x[:,1]
+        self.velocity_zs = input_x[:,2]
+        self.anglular_velocities = input_x[:,3]
+
+        self.pos_xs = input_y[:,0]
+        self.pos_zs = input_y[:,1]
+        self.angles = input_x[:,0] * np.pi
---
+++ data_manager_test.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import math
+import unittest
+
+from data_manager import DataManager
+
+class DataManagerTest(unittest.TestCase):
+    def setUp(self):
+        self.data_manager = DataManager()
+    
+    def test_value_range(self):
+        # Pos range is -4.5 ~ 4.5 (実際は-4.03~4.03あたり)
+        self.assertLessEqual(   np.max(self.data_manager.pos_xs),  4.5)
+        self.assertGreaterEqual(np.min(self.data_manager.pos_xs), -4.5)
+        
+        # Angle range is -pi ~ pi
+        self.assertLessEqual(   np.max(self.data_manager.angles),  np.pi)
+        self.assertGreaterEqual(np.min(self.data_manager.angles), -np.pi)
+        
+    def test_data_shape(self):
+        # Check data shape
+        self.assertEqual(self.data_manager.velocity_xs.shape,         (49999,))
+        self.assertEqual(self.data_manager.velocity_zs.shape,         (49999,))
+        self.assertEqual(self.data_manager.anglular_velocities.shape, (49999,))
+        
+        self.assertEqual(self.data_manager.angles.shape,              (49999,))
+        self.assertEqual(self.data_manager.pos_xs.shape,              (49999,))
+        self.assertEqual(self.data_manager.pos_zs.shape,              (49999,))
+        
+            
+if __name__ == '__main__':
+    unittest.main()
